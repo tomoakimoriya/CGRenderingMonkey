be604b09e2f645a652ff73e66b96285b8e66860f
moriya-univ-desktop <tomoriyahot@hotmail.com>
2022-10-18 15:50:45 +0900

    Remove verbose packages

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { ColladaLoader } from 'three/examples/jsm/loaders/ColladaLoader.js';
-import * as dat from "dat.gui";
-import * as Stats from "stats.js";
-
 
 class ThreeJSContainer {
     private scene: THREE.Scene;
